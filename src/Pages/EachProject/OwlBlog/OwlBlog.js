2b78cc94bcb295d1bce220b0cdf45af3df617ae0
Jeff Christian <jeffchristian.devfrontend@gmail.com>
2022-10-22 14:15:26 -0300

    added all images in use and stylized

--- src/Pages/EachProject/OwlBlog/OwlBlog.js
+++ src/Pages/EachProject/OwlBlog/OwlBlog.js
@@ -8,6 +8,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // Images
 import owlIcon from "../../../images/icon-owl.png";
 import owlHome from "../../../images/owl-principal-home.svg";
+import owlLogin from "../../../images/owl-principal-login.svg";
+import owlPost from "../../../images/owl-principal-post.svg";
+import owlProfile from "../../../images/owl-profille-left.svg";
+import owlEditDelete from "../../../images/owl-profile-right.svg"
+import owlEdit from "../../../images/owl-principal-edit.svg";
+import gitHub from "../../../images/github-crimson.svg";
 
 // Routes
 import { Link } from 'react-router-dom'
@@ -20,7 +26,7 @@ const OwlBlog = () => {
       <div className="flex_center">
 
         <div className={styles.slide_image}>
-          <img src={owlHome} alt="área principal de postagem" />
+          <img src={owlHome} alt="área principal de entrada" />
         </div>
 
         <div className={styles.description}>
@@ -35,18 +41,45 @@ const OwlBlog = () => {
           </div>
           <div className={styles.desc}>
             <p>
-              Breve descrição que daqui a pouco eu escrevo e penso melhor no quê colocar aqui...
+              Projeto criado com a inspiração do Facebook, a idéia é postar imagens com a motivação da postagem.
             </p>
           </div>
         </div>
-        <div className={styles.slide_image}></div>
-        <div className={styles.images_section}>
-          <div className={styles.images_left}></div>
-          <div className={styles.images_right}></div>
+
+        <div className={styles.slide_image}>
+          <img src={owlLogin} alt="área principal de login" />
         </div>
-        <div className={styles.slide_image}></div>
-        <div className={styles.slide_image}></div>
-        <div className={styles.description}></div>
+
+        <div className={styles.images_section}>
+
+          <div className={styles.images_left}>
+            <img src={owlProfile} alt="área principal de perfil" />
+          </div>
+          
+          <div className={styles.images_right}>
+            <img src={owlEditDelete} alt="área principal de edição e exclusão dos posts" />
+          </div>
+
+        </div>
+
+        <div className={styles.slide_image}>
+          <img src={owlPost} alt="área principal de postagem" />
+        </div>
+
+        <div className={styles.slide_image}>
+          <img src={owlEdit} alt="área principal de Edição de cada postagem" />
+        </div>
+
+        <div className={styles.description_bottom}>
+          <button className={styles.visit}>
+            <a href="https://jeff-christian.github.io/Owl-Blog/#/" target="_blank" rel="noreferrer">
+              Visitar Projeto
+            </a>
+          </button>
+          <a className={styles.github} href="https://github.com/Jeff-Christian/Owl-Blog" target="_blank" rel="noreferrer">
+            <img src={gitHub} alt="Acessar repositório" />
+          </a>
+        </div>
 
       </div>
     </div>
